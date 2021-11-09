b843007a9da47a051cac3c8611e940f8fabed27f
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-09 10:43:38 +0100

    Oefening 4.1 super klasse Figuur

---
+++ src/model/Figuur.java
@@ -0,0 +1,40 @@
+package model;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * Beschrijf de algemene eigenschappen van een figuur
+ */
+public class Figuur {
+    protected static final double GRENSWAARDE_GROOT_FIGUUR = 100.0;
+    protected static final String DEFAULTWAARDE_KLEUR = "groen";
+
+    protected String kleur;
+
+    public Figuur(String kleur) {
+        this.kleur = kleur;
+    }
+
+    public Figuur() {
+       this(DEFAULTWAARDE_KLEUR);
+    }
+
+    public static String geefDefinitie() {
+        return "Een figuur is een verzameling punten.";
+    }
+
+    public double geefOmtrek() {
+        return 0;
+    }
+
+    public double geefOppervlak() {
+        return 0;
+    }
+
+    public String vertelOverGrootte() {
+        if (geefOppervlak() > GRENSWAARDE_GROOT_FIGUUR) {
+            return "Ik ben groot!!!";
+        } else {
+            return "Ik ben klein!!!";
+        }
+    }
+}
