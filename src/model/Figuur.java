d728edcd58ef5e53c997da5ac9c4c3cf8255878a
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-09 14:58:45 +0100

    Oefening 5.2 abstract

--- src/model/Figuur.java
+++ src/model/Figuur.java
@@ -4,7 +4,7 @@ package model;
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
  * Beschrijf de algemene eigenschappen van een figuur
  */
-public class Figuur {
+public abstract class Figuur {
     protected static final double GRENSWAARDE_GROOT_FIGUUR = 100.0;
     protected static final String DEFAULT_KLEUR = "groen";
 
@@ -22,13 +22,9 @@ public class Figuur {
         return "Een figuur is een verzameling punten.";
     }
 
-    public double geefOmtrek() {
-        return 0;
-    }
+    public abstract double geefOmtrek();
 
-    public double geefOppervlakte() {
-        return 0;
-    }
+    public abstract double geefOppervlakte();
 
     public String vertelOverGrootte() {
         if (geefOppervlakte() > GRENSWAARDE_GROOT_FIGUUR) {
