a8847a30a6247c7f460f74c5e5e558d164f744a6
Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
2021-11-11 12:01:38 +0100

    Oefening 6.5 Arraylists en Oppervlak

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -1,9 +1,8 @@
 package controller;
 
-import model.Cirkel;
-import model.Figuur;
-import model.Punt;
-import model.Rechthoek;
+import model.*;
+
+import java.util.ArrayList;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -12,24 +11,26 @@ import model.Rechthoek;
 public class MeetkundeLauncher {
 
     public static void main(String[] args) {
-        Cirkel mijnCirkel = new Cirkel(3, new Punt(2, 5), "roze");
-        Rechthoek mijnRechthoek = new Rechthoek(3, 4, new Punt(-2, 6), "blauw");
+        Oppervlak oppervlak = new Oppervlak(10, 7);
+        oppervlak.voegFiguurToe(new Rechthoek(3, 3, new Punt(0, 7), "rood"));
+        oppervlak.voegFiguurToe(new Rechthoek(3, 2, new Punt(0, 4), "geel"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 2, new Punt(0, 2), "groen"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 2, new Punt(3, 7), "paars"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 4, new Punt(5, 7), "oranje"));
+        oppervlak.voegFiguurToe(new Rechthoek(5, 3, new Punt(5, 3), "blauw"));
 
-        Figuur[] figuren = new Figuur[3];
-        figuren[0] = mijnCirkel;
-        figuren[1] = mijnRechthoek;
-        figuren[2] = new Cirkel(10, new Punt(-1, -3), "karmozijn");
-
-        for (Figuur figuur : figuren) {
-            toonInformatie(figuur);
-            System.out.println();
-        }
-
-
+        toonInformatieAlleFiguren(oppervlak.geefFigurenMetGrotereOppervlakte(20));
     }
 
     public static void toonInformatie(Figuur figuur) {
         System.out.println(figuur.toString());
         System.out.println(figuur.vertelOverGrootte());
+    }
+
+    public static void toonInformatieAlleFiguren(ArrayList<Figuur> figuren) {
+        for (Figuur figuur : figuren) {
+            toonInformatie(figuur);
+            System.out.println();
+        }
     }
 }
---
+++ src/model/Oppervlak.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
+ * Beschrijft een oppervlak waar meerdere figuren op getoond kunnen worden.
+ */
+public class Oppervlak {
+    private double lengte;
+    private double breedte;
+    private ArrayList<Figuur> mijnFiguren;
+
+    public Oppervlak(double lengte, double breedte) {
+        this.lengte = lengte;
+        this.breedte = breedte;
+        mijnFiguren = new ArrayList<>();
+    }
+
+    public void voegFiguurToe(Figuur figuur) {
+        mijnFiguren.add(figuur);
+    }
+
+    @Override
+    public String toString() {
+        String eenReturn = "";
+
+        for (Figuur figuur : mijnFiguren) {
+            eenReturn += figuur.toString() + "\n\n";
+        }
+
+        return eenReturn;
+    }
+
+    public ArrayList<Figuur> geefFigurenMetGrotereOppervlakte(double grenswaarde) {
+        ArrayList<Figuur> gevraagdeFiguren = new ArrayList<>();
+
+        for (Figuur figuur : mijnFiguren) {
+            if (figuur.geefOppervlakte() > grenswaarde) {
+                gevraagdeFiguren.add(figuur);
+            }
+        }
+
+        return gevraagdeFiguren;
+    }
+
+    public ArrayList<Figuur> getMijnFiguren() {
+        return mijnFiguren;
+    }
+}
